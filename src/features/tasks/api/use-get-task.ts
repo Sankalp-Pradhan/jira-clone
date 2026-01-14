692c9b4487266b406e6eac74c30cef33e677fdac
Sankalp Pradhan <sankalppradhan1906@gmail.com>
2026-01-14 11:15:03 +0530

    built data filters

--- src/features/tasks/api/use-get-task.ts
+++ src/features/tasks/api/use-get-task.ts
@@ -1,19 +1,45 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { client } from "@/lib/rpc"
+import { TaskStatus } from "../types";
 
 interface UseGetTasksProps {
-    workspaceId: string,
+    workspaceId: string;
+    projectId?: string | null;
+    search?: string | null;
+    status?: string | null;
+    assigneeId?: string | null;
+    dueDate?: string | null;
+
 }
 
-export const useGetTask = ({
+export const useGetTasks = ({
     workspaceId,
+    projectId,
+    search,
+    assigneeId,
+    status,
+    dueDate
 }: UseGetTasksProps) => {
     const query = useQuery({
-        queryKey: ["tasks", workspaceId],
+        queryKey: ["tasks",
+            workspaceId,
+            projectId,
+            status,
+            search,
+            assigneeId,
+            dueDate,
+        ],
         queryFn: async () => {
             const response = await client.api.tasks.$get({
-                query: { workspaceId },
+                query: {
+                    workspaceId,
+                    projectId: projectId ?? undefined,
+                    status: status ?? undefined,
+                    search: search ?? undefined,
+                    assigneeId: assigneeId ?? undefined,
+                    dueDate: dueDate ?? undefined,
+                },
             });
 
             if (!response.ok) {
